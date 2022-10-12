7e4c65f4339e00e0dc8eeaf5e368fd8582993ab8
Belousov Nikolay <belousovnia@gmail.com>
2022-10-12 13:48:32 +0500

    add test validation 	modified:   package.json 	modified:   src/features/UserProfileForm/index.tsx 	new file:   src/ultils/validation/index.ts 	modified:   yarn.lock

--- src/features/UserProfileForm/index.tsx
+++ src/features/UserProfileForm/index.tsx
@@ -1,65 +1,59 @@
 import styles from './style.module.scss';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { Autocomplete, Typography } from '@mui/material';
-import { LocalizationProvider, MobileDatePicker } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { useState } from 'react';
-import dayjs, { Dayjs } from 'dayjs';
-import 'dayjs/locale/ru';
+import { Typography } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import { constructorErrorHelperText } from '../../ultils/validation';
 
-interface Citylabel {
-  label: string;
-}
+type FormValues = {
+  firstName: string;
+  lastName: string;
+};
 
 export default function UserForm() {
-  const [value, setValue] = useState<Dayjs | null>(dayjs('2022-09-10T21:11:54'));
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({ mode: 'onBlur' });
 
-  const handleChange = (newValue: Dayjs | null) => {
-    setValue(newValue);
+  const rules = {
+    required: true,
+    minLength: { value: 2, message: 'Поле должно содержать минимум 2 буквы.' },
+    pattern: { value: /^[а-яА-Я]*$/, message: 'Допускаются только русские буквы.' },
   };
 
-  const cities: Array<Citylabel> = [
-    { label: 'Москва' },
-    { label: 'Самара' },
-    { label: 'Томск' },
-    { label: 'Астрахань' },
-    { label: 'Якутск' },
-    { label: 'Зеленоград' },
-    { label: 'Норильск' },
-    { label: 'Киров' },
-    { label: 'Борск' },
-  ];
-
+  function onSubmit(date: FormValues) {
+    console.log(date);
+    alert('yes');
+  }
   return (
     <div className={styles.root}>
       <div className={styles.userForm}>
         <Typography className={styles.title} variant='h1'>
           Профиль пользователя
         </Typography>
-        <TextField className={styles.input} label='Имя' fullWidth />
-        <TextField className={styles.input} label='Фамилия' fullWidth />
-        <Autocomplete
-          className={styles.input}
-          disablePortal
-          id='autocomplete-city'
-          options={cities}
-          renderInput={params => <TextField {...params} label='Город' />}
-        />
-        <TextField className={styles.input} label='Лет стажа вождения' fullWidth type='number' />
-        <LocalizationProvider adapterLocale='ru' dateAdapter={AdapterDayjs}>
-          <MobileDatePicker
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <TextField
             className={styles.input}
-            label='Дата рождения'
-            inputFormat='MM.DD.YYYY'
-            value={value}
-            onChange={handleChange}
-            renderInput={params => <TextField {...params} />}
+            label='Имя'
+            fullWidth
+            {...register('firstName', rules)}
+            error={!!errors?.firstName}
+            helperText={constructorErrorHelperText(errors, 'firstName')}
           />
-        </LocalizationProvider>
-        <Button type='submit' size='large' variant='contained' fullWidth>
-          Продолжить
-        </Button>
+          <TextField
+            className={styles.input}
+            label='Фамилия'
+            fullWidth
+            {...register('lastName', rules)}
+            error={!!errors?.lastName}
+            helperText={constructorErrorHelperText(errors, 'lastName')}
+          />
+          <Button type='submit' size='large' variant='contained' fullWidth>
+            Продолжить
+          </Button>
+        </form>
       </div>
     </div>
   );
