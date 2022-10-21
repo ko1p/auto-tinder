f25597e092d2e874ab40c3db51e8f5af9232bc46
Belousov Nikolay <belousovnia@gmail.com>
2022-10-21 13:02:07 +0500

    add part features onboarding
    	modified:   package.json
    	deleted:    src/features/UserProfileForm/index.tsx
    	new file:   src/features/onboarding/components/CarForm/index.tsx
    	renamed:    src/features/UserProfileForm/slice.ts -> src/features/onboarding/components/CarForm/slice.ts
    	new file:   src/features/onboarding/components/CarForm/style.module.scss
    	new file:   src/features/onboarding/components/PhoneRequest/index.tsx
    	new file:   src/features/onboarding/components/PhoneRequest/slice.ts
    	renamed:    src/features/UserProfileForm/style.module.scss -> src/features/onboarding/components/PhoneRequest/style.module.scss
    	new file:   src/features/onboarding/index.ts
    	modified:   src/infrastructure/Routing/index.tsx
    	modified:   src/screens/MainScreen/index.tsx
    	modified:   src/store/rootReducer.ts
    	modified:   src/theming/styles/mixin-onboarding.scss
    	modified:   tsconfig.json
    	new file:   yarn-error.log
    	modified:   yarn.lock

---
+++ src/features/onboarding/index.ts
@@ -0,0 +1,5 @@
+import PhoneRequest from './components/PhoneRequest';
+import CarForm from './components/CarForm';
+
+export { PhoneRequest };
+export { CarForm };
--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,10 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import mainSlice from './mainSlice';
-import userProfile from '../features/UserProfileForm/slice';
+import CarForm from '../features/onboarding/components/CarForm/slice';
 import AuthSlice from '../features/Auth/slice';
 
 export const rootReducer = combineReducers({
   mainInfo: mainSlice,
   auth: AuthSlice,
-  userProfile: userProfile,
+  userProfile: CarForm,
 });
