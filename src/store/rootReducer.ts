22a3a094c620cddfb56faa11d709a13799cb99bf
Belousov Nikolay <belousovnia@gmail.com>
2022-10-21 15:21:56 +0500

    fix merge 	modified:   package.json 	modified:   src/store/rootReducer.ts 	modified:   yarn.lock

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,10 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import mainSlice from './mainSlice';
-import userAuthSlice from './userAuthSlice';
 import CarForm from '../features/onboarding/components/CarForm/slice';
+import AuthSlice from '../features/Auth/slice';
 
 export const rootReducer = combineReducers({
   mainInfo: mainSlice,
-  auth: userAuthSlice,
+  auth: AuthSlice,
   userProfile: CarForm,
 });
