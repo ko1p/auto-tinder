d14f7a925a4c95861d4bb58b8e667a6a362fd919
Айдар Шарафутдинов <shaidarharan87@gmail.com>
2022-11-02 22:22:50 +0300

    data: fix md

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
-function App() {
-  return (
-    <>Hello world!</>
-  );
-}
+const App = () => <>Hello world!</>;
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,11 @@ import 'normalize.css';
 import App from './app';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 reportWebVitals();
