20c6298b5aeefb0ee3f8893f2f46ee6c03e8df13
Айдар Шарафутдинов <shaidarharan87@gmail.com>
2022-11-02 22:34:24 +0300

    fix: lint

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,13 @@ import 'normalize.css';
 import App from './app';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 reportWebVitals();
